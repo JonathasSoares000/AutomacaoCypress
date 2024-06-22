cdab3ba479d31a875139641d51ec89e01ae1e763
AutomacaoCypress <jonathassoares@live.com>
2024-06-22 17:17:32 -0300

    Lista de Seletores Add

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -2,20 +2,31 @@
 
 
 describe('Orange HRM Tests', () => {
+
+const selectList = {
+
+username: "[name='username']",
+passeword: "[name='password']",
+btnCadastrar: "[type='submit']",
+
+
+}
+
+
   it('Login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin');
-    cy.get("[name='password']").type('admin123');
-    cy.get("[type='submit']").click();
+    cy.get(selectList.username).type('Admin');
+    cy.get(selectList.passeword).type('admin123');
+    cy.get(selectList.btnCadastrar).click();
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
     cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard');
   })
  
   it('Login Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test');
-    cy.get("[name='password']").type('test');
-    cy.get("[type='submit']").click();
+    cy.get(selectList.username).type('test');
+    cy.get(selectList.passeword).type('test');
+    cy.get(selectList.btnCadastrar).click();
     cy.get("[role='alert']").contains('Invalid credentials');
  
   })
